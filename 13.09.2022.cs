418c3f6a3474e999519b3f074fda288e72a786ac
Savenok <Romasav2017@gmail.com>
2022-09-14 16:35:08 +0300

    34

--- 13.09.2022.cs
+++ 13.09.2022.cs
@@ -1,41 +1,29 @@
-﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
-// [1 2 3 4 5] -> 5 8 3
-// [6 7 3 6] -> 36 21
+﻿//  Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 
+// [345, 897, 568, 234] -> 2
 Console.Clear();
 int[] GetArray(int size, int minValue, int maxValue)
 {
     int[] res = new int[size];
-    Console.Write("[");
+    Console.Write("[ ");
     for (int i = 0; i < size; i++)
     {
         res[i] = new Random().Next(minValue, maxValue + 1);
-        if (i == size - 1) Console.Write($"{res[i]}");
+        if (i == size-1) Console.Write($"{res[i]}");
         else Console.Write($"{res[i]} , ");
-    }
+    } 
     Console.Write("]");
     return res;
 }
-Console.WriteLine("   ");
-int[] GetMultiplication(int[] array2)
+int count = 0;
+int CountEvenNumbers(int[] arra)
 {
-    int[] end = new int[5];
-    int array2Lnegh = 0;
-    if (array2.Length%2 == 0) array2Lnegh = array2.Length / 2;
-    else array2Lnegh = array2.Length / 2 + 1;
-    end = new int[array2Lnegh];
-    for (int i = 0; i < array2Lnegh; i++)
-    {
-        if (i == array2Lnegh - 1) end[i] = array2[i];
-        else end[i] = array2[i] * array2[array2.Length - i - 1];
+    foreach(int i in arra){
+        if (i % 2 == 0) count ++;
+        else count += 0;
     }
-    return end;
+    Console.Write($" -> {count}");
+    return count;
 }
-void PrintArray(int[] array3)
-{
-    for (int i = 0; i < array3.Length; i++) Console.Write($" {array3[i]} ");
-}
-int[] array = GetArray(11, 1, 9);
-int[] array_2 = GetMultiplication(array);
-Console.Write("  -> ");
-PrintArray(array_2);
+int[] array = GetArray(6, 100 , 1000);
+CountEvenNumbers(array);
